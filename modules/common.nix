552db45c6d16398426e49876539f691ffce5a8cd
YJ Park <yjpark@gmail.com>
2025-04-26 19:17:08 +0800

    🚧 [WIP] - 2025-04-26 19:17:08 -

--- modules/common.nix
+++ modules/common.nix
@@ -4,6 +4,7 @@
     # Enables the OpenSSH server for remote access
     clan-core.clanModules.sshd
     clan-core.clanModules.state-version
+    ./system/boot.nix
     ./users/root.nix
     ./users/yjpark.nix
   ];
